dd48fca64dea7f8110183289682153f3e7d8d02e
MathewBarber <mbarber_84@hotmail.co.uk>
2025-10-14 08:59:50 +0100

    Functionality removed from poms

--- src/test/java/uk/co/twoitesting/twoitesting/pomclasses/LoginPOM.java
+++ src/test/java/uk/co/twoitesting/twoitesting/pomclasses/LoginPOM.java
@@ -1,53 +1,31 @@
-// Define the package where this class belongs
 package uk.co.twoitesting.twoitesting.pomclasses;
 
-// Import Selenium tools to interact with elements and wait for them
-import org.openqa.selenium.By; // Lets us find elements on the webpage
-import org.openqa.selenium.WebDriver; // Lets us control the browser
-import org.openqa.selenium.support.ui.ExpectedConditions; // Conditions to wait for
-import org.openqa.selenium.support.ui.WebDriverWait; // Wait until conditions are met
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import uk.co.twoitesting.twoitesting.actions.LoginActions;
 
-// Import NavPOM in case we need navigation tools (not directly used here)
-import uk.co.twoitesting.twoitesting.pomclasses.componentPOM.NavPOM;
-// Import helpers for config and utilities
-import uk.co.twoitesting.twoitesting.utilities.*;
+public class LoginPOM extends BasePOM {
 
-// Define the LoginPOM class to interact with the login page
-public class LoginPOM extends BasePOM { // Inherits useful functions from BasePOM
+    private final LoginActions loginActions;
 
-    // Locate the username and password fields and the login/logout buttons
-    private final By usernameField = By.id("username"); // Username input field
-    private final By passwordField = By.id("password"); // Password input field
-    private final By loginButton = By.name("login"); // Login button
-    private final By logoutButton = By.linkText("Logout"); // Logout link
-
-    // Constructor for LoginPOM
     public LoginPOM(WebDriver driver, WebDriverWait wait) {
-        super(driver, wait); // Call BasePOM constructor
+        super(driver, wait);
+        this.loginActions = new LoginActions(driver, wait);
     }
 
-    // Open the login page
     public void open() {
-        driver.get(ConfigLoader.get("base.url") + "/my-account/");
-        // Go to the my-account page using URL from config
+        loginActions.openLoginPage();
     }
 
-    // Perform login using credentials from config
     public void login() {
-        type(usernameField, ConfigLoader.get("username")); // Type username
-        type(passwordField, ConfigLoader.get("password")); // Type password
-        click(loginButton); // Click login button
+        loginActions.login();
     }
 
-    // Check if the user is currently logged in
     public boolean isUserLoggedIn() {
-        return isVisible(logoutButton); // If logout button is visible, user is logged in
+        return loginActions.isUserLoggedIn();
     }
 
-    // Log out if user is logged in
     public void logout() {
-        if (isUserLoggedIn()) { // Check if user is logged in
-            click(logoutButton); // Click logout button
-        }
+        loginActions.logout();
     }
 }
--- src/test/java/uk/co/twoitesting/twoitesting/pomclasses/ShopPOM.java
+++ src/test/java/uk/co/twoitesting/twoitesting/pomclasses/ShopPOM.java
@@ -1,97 +1,34 @@
-// This is the folder (package) where this class is stored
 package uk.co.twoitesting.twoitesting.pomclasses;
 
-// Import Allure tools for reporting steps and attaching files in test reports
-import io.qameta.allure.Allure; // Main Allure reporting class
-import io.qameta.allure.Step;   // To annotate steps for reporting
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import uk.co.twoitesting.twoitesting.actions.ShopActions;
 
-// Import Selenium tools to interact with the web page
-import org.openqa.selenium.*; // General Selenium classes (WebDriver, WebElement, etc.)
-import org.openqa.selenium.support.ui.ExpectedConditions; // Conditions to wait for
-import org.openqa.selenium.support.ui.WebDriverWait; // Wait until condition is true
-
-// Import PopUpPOM for handling popups
-import uk.co.twoitesting.twoitesting.pomclasses.componentPOM.PopUpPOM;
-// Import helpers for scrolling and other utility methods
-import uk.co.twoitesting.twoitesting.utilities.Helpers;
-
-// Import classes for reading files
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
-// Define ShopPOM class to control actions on the shop page
 public class ShopPOM {
 
-    // Browser controller
-    private final WebDriver driver;
-    // Wait controller to pause until elements are ready
-    private final WebDriverWait wait;
+    private final ShopActions shopActions;
 
-    // Find elements on the page
-    private final By shopLink = By.linkText("Shop"); // Link to go to the Shop page
-    private final By dismissBanner = By.linkText("Dismiss"); // Link to dismiss any popup/banner
-    private final By poloAddButton = By.cssSelector("li.product:nth-child(9) > a:nth-child(2)");
-    // Specific add-to-cart button for Polo shirt
-
-    // PopUp handler to manage popups on the page
-    private final PopUpPOM popupPom;
-
-    // Constructor runs when we create ShopPOM
     public ShopPOM(WebDriver driver, WebDriverWait wait) {
-        this.driver = driver; // Save the browser controller
-        this.wait = wait;     // Save the wait controller
-        this.popupPom = new PopUpPOM(driver, wait); // Initialize PopUpPOM for handling popups
+        this.shopActions = new ShopActions(driver, wait);
     }
 
-    // Open the Shop page, annotated as a step for Allure reporting
-    @Step("Open the Shop page")
     public void openShop() {
-        // Wait until the Shop link is clickable
-        WebElement shop = wait.until(ExpectedConditions.elementToBeClickable(shopLink));
-        // Scroll the Shop link into view
-        Helpers.scrollIntoView(driver, shop);
-        try {
-            shop.click(); // Try to click normally
-        } catch (ElementClickInterceptedException e) {
-            // If a popup blocks the click, click using JavaScript
-            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", shop);
-        }
+        shopActions.openShop();
     }
 
-    // Dismiss popup if it appears
     public void dismissPopupIfPresent() {
-        popupPom.dismissPopupIfPresent(); // Let PopUpPOM handle it
+        shopActions.dismissPopupIfPresent();
     }
 
-    // Add a product to the cart, product name comes from parameter
-    @Step("Add {productName} to cart")
     public void addProductToCart(String productName) {
-        // Build a dynamic CSS selector for the add-to-cart button
-        String ariaLabel = String.format("Add “%s” to your cart", productName);
-        By addButton = By.cssSelector("[aria-label='" + ariaLabel + "']");
-
-        // Wait until the button is present in the page
-        WebElement button = wait.until(ExpectedConditions.presenceOfElementLocated(addButton));
-        // Scroll button into view
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", button);
-        // Wait until clickable, then click
-        wait.until(ExpectedConditions.elementToBeClickable(button)).click();
+        shopActions.addProductToCart(productName);
     }
 
-    // Click the "View cart" link
-    @Step("View the cart")
     public void viewCart() {
-        wait.until(ExpectedConditions.elementToBeClickable(By.linkText("View cart"))).click();
+        shopActions.viewCart();
     }
 
-    // Attach a screenshot to Allure report
-    @Step("Attach screenshot: {name}")
     public void attachScreenshot(String name, String path) {
-        try (InputStream is = new FileInputStream(path)) { // Open screenshot file
-            Allure.addAttachment(name, is); // Attach screenshot to Allure
-        } catch (IOException e) { // Handle errors if file not found
-            e.printStackTrace(); // Print error for debugging
-        }
+        shopActions.attachScreenshot(name, path);
     }
 }
